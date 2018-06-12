71f592a791f8d585f4a5df017a094320d16b5abc
marvingommans <40215073+marvingommans@users.noreply.github.com>
2018-06-12 22:09:07 +0200

    Update .ino

--- .gitignore/.ino
+++ .gitignore/.ino
@@ -1,5 +1,6 @@
-int LED = 4; // digital pin D6
-int BUZZER = 6;
+int LED4 = 4; // digital pin D6
+int LED5 = 5;
+int LED6 = 6;
 int BUTTON = 2; // digital pin D2
 int buttonPressed = 0; // default value for times that button is pressed
 bool toggleButton = true; // var to check if the button has been clicked
@@ -9,13 +10,15 @@ unsigned long lastReset;
 void setup() {
   // Initialization of pinModes
   pinMode(BUTTON, INPUT);
-  pinMode(LED, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
   Serial.begin(9600);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
+    blinkMyLed();
   // read value of input pin
   if (digitalRead(BUTTON) == HIGH)
   {
@@ -31,10 +34,10 @@ void loop() {
     if (buttonPressed > 1)
     {
       // write outputvalues, set to high
-      digitalWrite(LED, HIGH);
-      digitalWrite(BUZZER, HIGH);   
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
       Serial.println("beweging");
-      delay (2000);
+      delay (5000);
       buttonPressed = 0;
     }
  
@@ -42,8 +45,7 @@ void loop() {
   else
   {
     // write outputvalues, set back to low
-    digitalWrite(LED, LOW);
-    digitalWrite(BUZZER, LOW);
+    digitalWrite(LED4, LOW);
     // button has been released, so trigger can be catched again in next loop
     toggleButton = true;
   }
@@ -53,3 +55,14 @@ void loop() {
             buttonPressed = 0;
     }
 }
+
+void blinkMyLed()
+{
+  static unsigned long lastBlinkTime = 0;
+  if (millis() - lastBlinkTime >= 1000UL)
+  {
+    digitalWrite(LED5, !digitalRead(LED5)); //toggles the led
+    lastBlinkTime = millis();
+  }
+}
+
